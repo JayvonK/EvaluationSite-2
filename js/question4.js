cd0fcfc6483e8d646d3a5ef91ff1f3b8f95c61a6
JayvonK <jaykhuth@gmail.com>
2024-02-20 03:12:05 -0800

    save

--- js/question4.js
+++ js/question4.js
@@ -19,35 +19,261 @@ let decimalBtn = document.getElementById("decimalBtn");
 let equalsBtn = document.getElementById("equalsBtn");
 let userInput = document.getElementById("userInput");
 
-let numOne = "";
-let numTwo = "";
+let init = true;
+let numOne = "0";
+let numTwo = "0";
 let first = false;
-let dot = false;
+let add = false;
+let subtract = false;
+let divide = false;
+let multiply = false;
 let count = 0;
 
 ACBtn.addEventListener('click', () => {
-    numOne = 0;
-    numTwo = 0;
+    numOne = "0";
+    numTwo = "0";
     first = true;
+    init = true;
     userInput.value = "0";
-    dot = false;
+    add = false;
+    subtract = false;
+    divide = false;
+    multiply = false;
+    count = 0;
 })
+
+
 
 oneBtn.addEventListener('click', () => {
     if (!first) {
-        numOne += "1";
-        userInput.value = numOne;
-    }  else {
-        numTwo += "1";
-        userInput.value = numTwo;
+        if (init) {
+            numOne = "1";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "1";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "1";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "1";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+twoBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "2";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "2";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "2";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "2";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+threeBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "3";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "3";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "3";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "3";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+fourBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "4";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "4";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "4";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "4";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+fiveBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "5";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "5";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "5";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "5";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+sixBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "6";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "6";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "6";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "6";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+sevenBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "7";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "7";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "7";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "7";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+eightBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "8";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "8";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "8";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "8";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+nineBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "9";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "9";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "9";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "9";
+            userInput.value = numTwo;
+        }
+    }
+})
+
+zeroBtn.addEventListener('click', () => {
+    if (!first) {
+        if (init) {
+            numOne = "0";
+            userInput.value = numOne;
+            init = false
+        } else {
+            numOne += "0";
+            userInput.value = numOne;
+        }
+    } else {
+        if (init) {
+            numTwo = "0";
+            userInput.value = numTwo;
+            init = false;
+        } else {
+            numTwo += "0";
+            userInput.value = numTwo;
+        }
     }
 })
 
 
 decimalBtn.addEventListener('click', () => {
-    count++;
     if (count < 1) {
-        dot = true;
-        userInput.value += ".";
+        if(!first){
+            numOne += ".";
+            userInput.value = numOne;
+        } else {
+            numTwo += ".";
+            userInput.value = numTwo;
+        }
     }
+    count++;
 })
