61ce85765f4c9bb3012b183b045c955b7efa0897
JayvonK <jaykhuth@gmail.com>
2024-02-19 23:17:09 -0800

    fixing

--- js/question4.js
+++ js/question4.js
@@ -19,199 +19,152 @@ let decimalBtn = document.getElementById("decimalBtn");
 let equalsBtn = document.getElementById("equalsBtn");
 let userInput = document.getElementById("userInput");
 
-let numOne = 0;
-let numTwo = 0;
+let init = true;
+let numOne;
+let numTwo;
 let first = false;
 let dot = false;
+let count = 0;
 
 ACBtn.addEventListener('click', () => {
     numOne = 0;
     numTwo = 0;
+    init = true;
     first = true;
     userInput.value = 0;
     dot = false;
 })
 
 oneBtn.addEventListener('click', () => {
-    if(!first){
-        numOne = 1;
-        userInput.value = 1;
-        first = true;
-        if(dot){
-            userInput.value += 1;
+    if (!first) {
+        if (init) {
+            numOne = "1";
+            userInput.value = numOne;
+        } else {
+            numOne += "1";
+            userInput.value = numOne;
         }
+        init = false;
     } else {
-        numTwo = 1;
-        userInput.value = 1;
-        first = false;
-        if(dot){
-            userInput.value += 1;
+        if (init) {
+            numTwo = "1";
+            userInput.value = numTwo;
+        } else {
+            numTwo += "1";
+            userInput.value = numTwo;
         }
+        init = false;
     }
 })
 
 twoBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 2;
         userInput.value = 2;
         first = true;
-        if(dot){
-            userInput.value += 2;
-        }
     } else {
         numTwo = 2;
         userInput.value = 2;
         first = false;
-        if(dot){
-            userInput.value += 2;
-        }
     }
 })
 
 threeBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 3;
         userInput.value = 3;
         first = true;
-        if(dot){
-            userInput.value += 3;
-        }
     } else {
         numTwo = 3;
         userInput.value = 3;
         first = false;
-        if(dot){
-            userInput.value += 3;
-        }
     }
 })
 
 fourBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 4;
         userInput.value = 4;
         first = true;
-        if(dot){
-            userInput.value += 4;
-        }
     } else {
         numTwo = 4;
         userInput.value = 4;
         first = false;
-        if(dot){
-            userInput.value += 4;
-        }
     }
 })
 
 fiveBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 5;
         userInput.value = 5;
         first = true;
-        if(dot){
-            userInput.value += 5;
-        }
     } else {
         numTwo = 5;
         userInput.value = 5;
         first = false;
-        if(dot){
-            userInput.value += 5;
-        }
     }
 })
 
 sixBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 6;
         userInput.value = 6;
         first = true;
-        if(dot){
-            userInput.value += 6;
-        }
     } else {
         numTwo = 6;
         userInput.value = 6;
         first = false;
-        if(dot){
-            userInput.value += 6;
-        }
     }
 })
 
 sevenBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 7;
         userInput.value = 7;
         first = true;
-        if(dot){
-            userInput.value += 7;
-        }
     } else {
         numTwo = 7;
         userInput.value = 7;
         first = false;
-        if(dot){
-            userInput.value += 7;
-        }
     }
 })
 
 eightBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 8;
         userInput.value = 8;
         first = true;
-        if(dot){
-            userInput.value += 8;
-        }
     } else {
         numTwo = 8;
         userInput.value = 8;
         first = false;
-        if(dot){
-            userInput.value += 8;
-        }
     }
 })
 
 nineBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 9;
         userInput.value = 9;
         first = true;
-        if(dot){
-            userInput.value += 9;
-        }
     } else {
         numTwo = 9;
         userInput.value = 9;
         first = false;
-        if(dot){
-            userInput.value += 9;
-        }
     }
 })
 
 zeroBtn.addEventListener('click', () => {
-    if(!first){
+    if (!first) {
         numOne = 0;
         userInput.value = 0;
         first = true;
-        if(dot){
-            userInput.value += 0;
-        }
     } else {
         numTwo = 0;
         userInput.value = 0;
         first = false;
-        if(dot){
-            userInput.value += 0;
-        }
     }
 })
 
 decimalBtn.addEventListener('click', () => {
-    dot = true;
+    
 })
